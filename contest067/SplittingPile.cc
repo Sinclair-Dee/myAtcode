db1bc254ae8ef4ed0fad5732aca24295121a1138
SinclairDee <dixinkai@outlook.com>
2017-07-16 10:40:36 +0000

    contest 067

--- contest067/SplittingPile.cc
+++ contest067/SplittingPile.cc
@@ -10,15 +10,17 @@ int main()
 	cin>>l[i];
 	sum +=l[i];
     }
-    // operation procssing
+    // operation processing
 	long long sum0 = 0;
 	long long temp =	1000000000000000;
 	for(i= 0;i<n-1;i++){
 		sum0 +=l[i];
-	if(2*sum0-sum>=0)
-		temp= (2*sum0-sum)<temp?(2*sum0-sum):temp;
-	else 
-		temp= (sum-2*sum0)<temp?(sum-2*sum0):temp;
+	temp = (std::abs(2*sum0-sum))<temp?(std::abs(2*sum0-sum)):temp;
+	//可以直接调用std::abs
+	//if(2*sum0-sum>=0)
+	//	temp= (2*sum0-sum)<temp?(2*sum0-sum):temp;
+	//else 
+	//	temp= (sum-2*sum0)<temp?(sum-2*sum0):temp;
 	}
 	cout<<temp<<endl;
     return 0;
