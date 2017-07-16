7fd1b80f0b7700b5de95550450db1988a8719ada
SinclairDee <dixinkai@outlook.com>
2017-07-16 08:55:42 +0000

    contest067

---
+++ contest067/SplittingPile.cc
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k,n;
+    cin >>n;
+    int l[n];
+	long long sum = 0;
+    for (i = 0;i<n;i++){
+	cin>>l[i];
+	sum +=l[i];
+    }
+    // output
+	long long sum0 = 0;
+	long long temp =	1000000000000000;
+	for(i= 0;i<n-1;i++){
+		sum0 +=l[i];
+	if(2*sum0-sum>=0)
+		temp= (2*sum0-sum)<temp?(2*sum0-sum):temp;
+	else 
+		temp= (sum-2*sum0)<temp?(sum-2*sum0):temp;
+	}
+	cout<<temp<<endl;
+    return 0;
+}
