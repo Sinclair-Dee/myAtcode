e1d6fc486104053b958440a1aad0d2971562aff9
SinclairDee <dixinkai@outlook.com>
2017-08-05 14:26:01 +0000

    contest067

---
+++ contest067/Decrease.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+long long num[50];
+ 
+int main()
+{
+    long long k;
+    cin >> k;
+    long long t = k / 50;
+    for(int i = 0;i < 50;i ++) num[i] = k / 50;
+    k %= 50;
+    for(int i = 0;i < 50;i ++)
+    {
+        if(i != (50 - k)) num[i] += i;
+        else num[i] += 50;
+    }
+       printf("50\n");
+    for(int i = 0;i < 49;i ++) printf("%lld ",num[i]);
+    printf("%lld\n",num[49]);
+    return 0;
+}
